348583aa04c5500f63b245cf1d45b1d2aa134a60
oli-lane <markus.oliver.lane@gmail.com>
2023-04-25 11:37:27 -0700

    UI packages

--- FlowGoals/App.tsx
+++ FlowGoals/App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "react-native-rapi-ui";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme="light">
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
