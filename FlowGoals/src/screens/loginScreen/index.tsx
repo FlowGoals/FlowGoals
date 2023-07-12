c971ab3113d2b764122b7d60a3e074f4494b49b8
oli-lane <markus.oliver.lane@gmail.com>
2023-07-11 21:36:29 -0700

    auth FE

--- FlowGoals/src/screens/loginScreen/index.tsx
+++ FlowGoals/src/screens/loginScreen/index.tsx
@@ -1,11 +1,13 @@
 import React, { useContext, useState } from 'react';
 import {
-  View, TextInput, Image, StyleSheet,
+  View, TextInput, Image, StyleSheet, Text,
 } from 'react-native';
 import { Button } from 'react-native-rapi-ui';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ionicons } from '@expo/vector-icons';
 import AuthContext from '../../context/AuthContext';
 import { LoginProp } from '../../navigation/types';
+import { colors } from '../../components/utils/Colors';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,13 +17,28 @@ const styles = StyleSheet.create({
     padding: 20,
     rowGap: 10,
   },
-  input: {
+  inputContainer: {
     width: '100%',
     height: 40,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
     paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+  },
+  inputError: {
+    borderColor: 'red',
+  },
+  errorText: {
+    color: 'red',
+  },
+  navText: {
+    color: colors.blue1dark,
+    fontSize: 16,
   },
 });
 
@@ -30,12 +47,20 @@ function LoginScreen({ navigation }: LoginProp) {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = async () => {
     try {
-      await login({ username, password });
-      AsyncStorage.setItem('username', username);
-      AsyncStorage.setItem('password', password);
+      const loginSuccess = await login({ username, password });
+      if (loginSuccess !== undefined) {
+        if (loginSuccess) {
+          AsyncStorage.setItem('username', username);
+          AsyncStorage.setItem('password', password);
+        } else {
+          setIsError(true);
+        }
+      }
     } catch (err) {
       console.log(err);
     }
@@ -48,21 +73,33 @@ function LoginScreen({ navigation }: LoginProp) {
         source={require('../../assets/icon.png')}
         style={{ width: 150, height: 150 }}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button text="Login" onPress={handleLogin} />
-      <Button text="go to sign up" onPress={() => navigation.navigate('Signup')} />
+      <View style={[styles.inputContainer, isError && styles.inputError]}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+      </View>
+      <View style={[styles.inputContainer, isError && styles.inputError]}>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={!showPassword}
+          value={password}
+          onChangeText={setPassword}
+        />
+        <Ionicons
+          name={showPassword ? 'eye-off' : 'eye'}
+          size={24}
+          color="#888"
+          onPress={() => setShowPassword(!showPassword)}
+        />
+      </View>
+      {isError && <Text style={styles.errorText}>Incorrect username or password</Text>}
+      <Button text="Login" onPress={handleLogin} color={colors.blue1dark} />
+      <Text style={styles.navText} onPress={() => navigation.navigate('Signup')}>Go to sign up</Text>
+
     </View>
   );
 }
